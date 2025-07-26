040a7d1b23094f542807823a1633b022aa999cdf
Vedant <vedant28j@gmail.com>
2025-07-26 16:36:11 +0000

    Comments

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // console.log('Hello, dev');
 const fs=require('fs')
 
-fs.readFile('example.txt','utf-8',(err,data)=>{
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log(data);
-})
+// fs.readFile('example.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// })
