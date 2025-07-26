773dce9950d813dfda46e536e4609720b829ebea
Vedant <vedant28j@gmail.com>
2025-07-26 16:28:19 +0000

    Reading data From file system using NODE

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log('Hello, dev');
+const fs=require('fs')
+
+fs.readFile('example.txt','utf-8',(err,data)=>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log(data);
+})
