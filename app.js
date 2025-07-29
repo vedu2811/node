f4575d5107cf11863bdafa4e3a7dbca88d42d441
Vedant <vedant28j@gmail.com>
2025-07-29 23:38:19 +0000

    checking if a diretory exists or not

--- app.js
+++ app.js
@@ -169,3 +169,10 @@ const lodash = require('lodash')
 
 // const files=fs.readdirSync('./')
 // console.log('Directory Content', files);
+
+const dirName='newDir3'
+if(fs.existsSync(dirName)){
+    console.log('Dir Exists');
+}else{
+    console.log('Dir does not exist');
+}
