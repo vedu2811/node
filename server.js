0fc6b9ffaf3d1e11c411b6755854a00289508cef
Vedant <vedant28j@gmail.com>
2025-08-01 22:41:44 +0000

    Handling Dynamic Routes

--- server.js
+++ server.js
@@ -19,7 +19,15 @@ const url = require('url')
 
 const server = http.createServer((req,res)=>{
     
-    
+    const {pathname}=url.parse(req.url)
+    if(pathname.startsWith('/user/')){
+        const userId = pathname.split('/')[2]
+        res.writeHead(200,{'content-type':'text/plain'})
+        res.end(`User Id : ${userId}`)
+    }else{
+        res.writeHead(404,{'content-type':'text/plain'})
+        res.end('Route Not Found')
+    }
 
 })
 
