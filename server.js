91cebe6a234411886ac16b0b79781630090d49ad
Vedant <vedant28j@gmail.com>
2025-08-01 20:47:56 +0000

    req and res methods

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http=require('http')
 
 const server = http.createServer((req,res)=>{
-    
+    if(req.method==='GET' && req.url==='/'){
+        res.writeHead(200,{'content-type':'text/plain'})
+        res.end('Welcome to HomePage')
+    } else{
+        res.writeHead(404,{'content-type':'text/plain'})
+        res.end('Page Not Found')
+    }
 })
 
 const port=3000
