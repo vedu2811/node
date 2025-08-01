c8327488a46541cfdc4e05afeda379ecfcc697bc
Vedant <vedant28j@gmail.com>
2025-08-01 23:17:46 +0000

    Comments

--- server.js
+++ server.js
@@ -18,23 +18,13 @@ const url = require('url')
 // }
 
 // Middleware Function for logging requests
-const logRequest = ((req,res,next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next(req,res)
-})
+// const logRequest = ((req,res,next) => {
+//     console.log(`${req.method} request made to ${req.url}`);
+//     next(req,res)
+// })
 
 const server = http.createServer((req,res)=>{
-    logRequest(req,res,(req,res)=>{
-        const {pathname}=url.parse(req.url)
-        if(pathname.startsWith('/user/')){
-            const userId = pathname.split('/')[2]
-            res.writeHead(200,{'content-type':'text/plain'})
-            res.end(`User Id : ${userId}`)
-        }else{
-            res.writeHead(404,{'content-type':'text/plain'})
-            res.end('Route Not Found')
-        }
-    })
+    
 })
 
 const port=3000
