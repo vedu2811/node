5e0361786c119cf442414d47f7f22f8c6049d45c
Vedant <vedant28j@gmail.com>
2025-08-01 20:41:04 +0000

    Setting up a HTTP SERVER

---
+++ server.js
@@ -0,0 +1,11 @@
+const http=require('http')
+
+const server = http.createServer((req,res)=>{
+    
+})
+
+const port=3000
+
+server.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
