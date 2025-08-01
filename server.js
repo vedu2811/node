99848327da008b620cc9c146afa1ab40e6693b3f
Vedant <vedant28j@gmail.com>
2025-08-01 21:02:53 +0000

    Handling POST Requests

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const http=require('http')
 
 const server = http.createServer((req,res)=>{
-    // Request is GET Method
-    if(req.method==='GET' && req.url==='/'){
-        res.writeHead(200,{'content-type':'text/plain'})
-        res.end('Welcome to HomePage')
+    // Request is POST Method
+    if(req.method==='POST' && req.url==='/submit'){
+
+        let body=''
+
+        req.on('data',(chunk)=>{
+            body+=chunk.toString()
+        })
+
+        req.on('end',()=>{
+            res.writeHead(200,{'content-type':'application/json'})
+            res.end(JSON.stringify({message:'Data received',data:body}))
+        })
+
     } else{
-        res.writeHead(404,{'content-type':'text/plain'})
-        res.end('Page Not Found')
+        res.writeHead(404,{"content-type":'text/plain'})
+        res.end('Route Not Found')
     }
 })
 
